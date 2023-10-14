cc5af8a674c40936f751d048287a00663e67e384
Jonah <jonahm@bu.edu>
2023-10-14 18:54:31 -0400

    It is shaking for the east activity

--- app/src/main/java/com/example/swipedetect/EastActivity.kt
+++ app/src/main/java/com/example/swipedetect/EastActivity.kt
@@ -1,11 +1,65 @@
 package com.example.swipedetect
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.Toast
+import android.widget.Toast.makeText
+import kotlinx.coroutines.delay
+
+import java.util.Objects
+import kotlin.math.sqrt
 
 class EastActivity : AppCompatActivity() {
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+    private lateinit var eastpic: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_east)
+
+        eastpic = findViewById(R.id.eastpic)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+    }
+    private val sensorListener: SensorEventListener = object : SensorEventListener{
+        override fun onSensorChanged(event: SensorEvent){
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            if (acceleration > 10) {
+                eastpic.startAnimation(AnimationUtils.loadAnimation(this@EastActivity, R.anim.shake))
+
+                eastpic.postDelayed({
+                    eastpic.clearAnimation()
+                }, 2000)
+            }
+        }
+
+        override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        }
     }
 }
