f984c99418d92710bc78eca2f37f5fb387753616
makbu324 <angusmak@bu.edu>
2023-10-15 23:17:19 -0400

    Image JUNit Tests added

--- app/src/test/java/com/example/swipedetect/ExampleUnitTest.kt
+++ app/src/test/java/com/example/swipedetect/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.example.swipedetect
 
-import org.junit.Test
-
+import android.widget.ImageView
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.Test.None
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,26 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    //Mak: 4 JUnit tests for detecting if each side's images exist before running program
+    @Test
+    fun EastImageExists() {
+        assertNotEquals(R.drawable.trees, null)
+    }
+
+    @Test
+    fun WestImageExists() {
+        assertNotEquals(R.drawable.desert, null)
+    }
+
+    @Test
+    fun SouthImageExists() {
+        assertNotEquals(R.drawable.beach, null)
+    }
+
+    @Test
+    fun NorthImageExists() {
+        assertNotEquals(R.drawable.snowymountain, null)
     }
 }
